f828d0a3f6aa63c944a5479bdf4ac21615152a18
Svet Bajlekov <tomatoman@gmail.com>
2022-01-31 15:27:35 +0100

    Switch to using rumqttc

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,21 @@
-use std::{
-    env,
-    process,
-    time::Duration
-};
+use rumqttc::{MqttOptions, Client, QoS};
+use std::time::Duration;
+use std::thread;
 
-extern crate paho_mqtt as mqtt;
+pub fn mqtt_publish(count: u8) {
+    let mut mqttoptions = MqttOptions::new("rumqtt-sync", "localhost", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
 
-const DFLT_BROKER:&str = "tcp://localhost:1883";
-const DFLT_CLIENT:&str = "rust_publish";
-const DFLT_TOPICS:&[&str] = &["rust/mqtt", "rust/test"];
-// Define the qos.
-const QOS:i32 = 1;
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
 
-pub fn mqtt_publish(count: u16) {
-    let host = env::args().nth(1).unwrap_or_else(||
-        DFLT_BROKER.to_string()
-    );
-
-    // Define the set of options for the create.
-    // Use an ID for a persistent session.
-    let create_opts = mqtt::CreateOptionsBuilder::new()
-        .server_uri(host)
-        .client_id(DFLT_CLIENT.to_string())
-        .finalize();
-
-    // Create a client.
-    let cli = mqtt::Client::new(create_opts).unwrap_or_else(|err| {
-        println!("Error creating the client: {:?}", err);
-        process::exit(1);
+    thread::spawn(move || for i in 0..count {
+        client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).unwrap();
+        thread::sleep(Duration::from_millis(100));
     });
 
-    // Define the set of options for the connection.
-    let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(Duration::from_secs(20))
-        .clean_session(true)
-        .finalize();
-
-    // Connect and wait for it to complete or fail.
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Unable to connect:\n\t{:?}", e);
-        process::exit(1);
+    // Iterate to poll the eventloop for connection progress
+    for (_, notification) in connection.iter().enumerate() {
+        println!("Notification = {:?}", notification);
     }
-
-    // Create a message and publish it.
-    // Publish message to 'test' and 'hello' topics.
-    for num in 0..count {
-        let content =  "Hello world! ".to_string() + &num.to_string();
-        let mut msg = mqtt::Message::new(DFLT_TOPICS[0], content.clone(), QOS);
-        if num % 2 == 0 {
-            println!("Publishing messages on the {:?} topic", DFLT_TOPICS[1]);
-            msg = mqtt::Message::new(DFLT_TOPICS[1], content.clone(), QOS);
-        } else {
-            println!("Publishing messages on the {:?} topic", DFLT_TOPICS[0]);
-        }
-        let tok = cli.publish(msg);
-
-                if let Err(e) = tok {
-                        println!("Error sending message: {:?}", e);
-                        break;
-                }
-    }
-
-
-    // Disconnect from the broker.
-    let tok = cli.disconnect(None);
-    println!("Disconnect from the broker");
-    tok.unwrap();
 }
  
